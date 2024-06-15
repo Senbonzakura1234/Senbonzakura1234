82d1723e16407db6fb0539f77351d1183f9c82bc
Pham Anh Dung <anhdungpham090@gmail.com>
2024-06-15 23:31:50 +0700

    Update Readme with husky cli.

---
+++ .eslintrc.cjs
@@ -0,0 +1,85 @@
+/** @type {Partial<import('eslint-define-config').Override>} */
+const modernCodeConfig = {
+	parserOptions: { project: './tsconfig.json', ecmaVersion: 'latest', sourceType: 'module' },
+	parser: '@typescript-eslint/parser'
+};
+
+/** @type {Partial<import('eslint-define-config').Override>} */
+const cjsCodeConfig = {
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	parser: '@typescript-eslint/parser'
+};
+
+/** @type {Partial<import('eslint-define-config').Rules>} */
+const commonCodeRules = {
+	'@typescript-eslint/comma-dangle': 'warn',
+	'@typescript-eslint/no-unused-vars': 'off'
+};
+
+/** @type {Partial<import('eslint-define-config').Rules>} */
+const modernCodeRules = {
+	'@typescript-eslint/consistent-type-imports': 'warn',
+	'@typescript-eslint/no-floating-promises': 'warn',
+	'@typescript-eslint/require-await': 'off',
+	'@typescript-eslint/unbound-method': 'off',
+	'@typescript-eslint/no-unnecessary-type-assertion': 'off',
+	'@typescript-eslint/no-misused-promises': [
+		'error',
+		{
+			checksConditionals: true,
+			checksSpreads: true,
+			checksVoidReturn: { arguments: true, attributes: false, properties: false, returns: true, variables: false }
+		}
+	],
+	...commonCodeRules
+};
+
+const cjsCodeRules = { '@typescript-eslint/no-var-requires': 'off', ...commonCodeRules };
+
+/** @type {Partial<import('eslint-define-config').Rules>} */
+const importRules = {
+	'sort-imports': [
+		'warn',
+		{
+			allowSeparatedGroups: true,
+			ignoreCase: false,
+			ignoreDeclarationSort: true,
+			ignoreMemberSort: false,
+			memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+		}
+	],
+	'unused-imports/no-unused-imports': 'warn',
+	'unused-imports/no-unused-vars': [
+		'warn',
+		{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+	]
+};
+
+/** @type {import('eslint-define-config').ESLintConfig} */
+module.exports = {
+	extends: ['plugin:prettier/recommended'],
+	rules: { 'prettier/prettier': 'warn' },
+	overrides: [
+		{
+			extends: ['plugin:@typescript-eslint/recommended-type-checked'],
+			files: ['**/*.ts', '**/*.tsx', '**/*.mjs'],
+			...modernCodeConfig,
+			plugins: ['unused-imports'],
+			rules: { ...modernCodeRules, ...importRules }
+		},
+		{
+			extends: ['plugin:@typescript-eslint/recommended'],
+			files: ['**/*.cjs'],
+			...cjsCodeConfig,
+			plugins: ['unused-imports'],
+			rules: { ...cjsCodeRules, ...importRules }
+		},
+		{
+			extends: ['plugin:markdown/recommended-legacy'],
+			files: ['**/*.md', '**/*.mdx'],
+			processor: 'markdown/markdown'
+		},
+		{ extends: ['plugin:json/recommended-legacy'], files: ['**/*.json'] }
+	],
+	ignorePatterns: ['node_modules', 'dist']
+};
---
+++ src/types/global.d.ts
@@ -0,0 +1,13 @@
+declare module 'mdx-to-md' {
+	async function mdxToMd<
+		Frontmatter extends {
+			[key: string]: unknown;
+		}
+	>(
+		/** The path to the MDX file. */
+		path: string,
+
+		/** Configure internal library options. */
+		options?: Pick<BundleMDX<Frontmatter>, 'esbuildOptions' | 'grayMatterOptions' | 'mdxOptions'>
+	): Promise<string>;
+}
